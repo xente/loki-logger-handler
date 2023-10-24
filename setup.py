ee8cea4777bd4d693dd4bfb969091df92eb105c4
Vicente Vazquez <vicente.vazquez@colba.es>
2023-10-24 09:19:51 +0200

    Add correct setup configuration

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/xente/loki-logger-handler",
-    packages=["loki_logger_handler"],
+    packages=find_packages(),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
