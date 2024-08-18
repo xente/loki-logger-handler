e4b018723587f7588a7388032d47b3c1d0d9164d
Norman Valerio <novama@gmail.com>
2024-08-18 01:18:28 -0600

    Reorganize the sequence of arguments in the LokiLoggerHandler constructor (useful change when loading logging configuration from files using `logging.config.fileConfig()`)

--- loki_logger_handler/loki_logger_handler.py
+++ loki_logger_handler/loki_logger_handler.py
@@ -11,46 +11,94 @@ from loki_logger_handler.stream import Stream
 
 
 class LokiLoggerHandler(logging.Handler):
+    """
+    A custom logging handler that sends logs to a Loki server.
+
+    Attributes:
+        labels (dict): Default labels for the logs.
+        label_keys (dict): Specific log record keys to extract as labels.
+        timeout (int): Timeout interval for flushing logs.
+        logger_formatter (logging.Formatter): Formatter for log records.
+        request (LokiRequest): Loki request object for sending logs.
+        buffer (queue.Queue): Buffer to store log records before sending.
+        flush_thread (threading.Thread): Thread for periodically flushing logs.
+    """
+
     def __init__(
         self,
         url,
         labels,
-        labelKeys=None,
+        label_keys=None,
+        additional_headers=None,
         timeout=10,
         compressed=True,
-        defaultFormatter=LoggerFormatter(),
-        additional_headers=dict()
+        loguru=False,
+        default_formatter=None
     ):
+        """
+        Initialize the LokiLoggerHandler object.
+
+        Args:
+            url (str): The URL of the Loki server.
+            labels (dict): Default labels for the logs.
+            label_keys (dict, optional): Specific log record keys to extract as labels. Defaults to an empty dictionary.
+            additional_headers (dict, optional): Additional headers for the Loki request. Defaults to an empty dict.
+            timeout (int, optional): Timeout interval for flushing logs. Defaults to 10 seconds.
+            compressed (bool, optional): Whether to compress the logs using gzip. Defaults to True.
+            loguru (bool, optional): Whether to use LoguruFormatter. Defaults to False.
+            default_formatter (logging.Formatter, optional): Formatter for the log records. If not provided,
+                                                             LoggerFormatter or LoguruFormatter will be used.
+        """
         super().__init__()
 
-        if labelKeys is None:
-            labelKeys = {}
-
         self.labels = labels
-        self.labelKeys = labelKeys
+        self.label_keys = label_keys if label_keys is not None else {}
         self.timeout = timeout
-        self.logger_formatter = defaultFormatter
-        self.request = LokiRequest(url=url, compressed=compressed, additional_headers=additional_headers)
+        self.logger_formatter = default_formatter if default_formatter is not None else (
+            LoguruFormatter() if loguru else LoggerFormatter()
+        )
+        self.request = LokiRequest(url=url, compressed=compressed, additional_headers=additional_headers or {})
         self.buffer = queue.Queue()
         self.flush_thread = threading.Thread(target=self._flush, daemon=True)
         self.flush_thread.start()
 
     def emit(self, record):
-        self._put(self.logger_formatter.format(record))
+        """
+        Emit a log record.
 
+        Args:
+            record (logging.LogRecord): The log record to be emitted.
+        """
+        # noinspection PyBroadException
+        try:
+            formatted_record = self.logger_formatter.format(record)
+            self._put(formatted_record)
+        except Exception:
+            # Silently ignore any exceptions
+            pass
+
+    # noinspection PyBroadException
     def _flush(self):
+        """
+        Flush the buffer by sending the logs to the Loki server.
+        This function runs in a separate thread and periodically sends logs.
+        """
         atexit.register(self._send)
 
-        flushing = False
         while True:
-            if not flushing and not self.buffer.empty():
-                flushing = True
-                self._send()
-                flushing = False
+            if not self.buffer.empty():
+                try:
+                    self._send()
+                except Exception:
+                    # Silently ignore any exceptions
+                    pass
             else:
                 time.sleep(self.timeout)
 
     def _send(self):
+        """
+        Send the buffered logs to the Loki server.
+        """
         temp_streams = {}
 
         while not self.buffer.empty():
@@ -59,31 +107,69 @@ class LokiLoggerHandler(logging.Handler):
                 stream = Stream(log.labels)
                 temp_streams[log.key] = stream
 
-            temp_streams[log.key].appendValue(log.line)
+            temp_streams[log.key].append_value(log.line)
 
         if temp_streams:
-            streams = Streams(temp_streams.values())
+            streams = Streams(list(temp_streams.values()))
             self.request.send(streams.serialize())
 
     def write(self, message):
+        """
+        Write a message to the log.
+
+        Args:
+            message (str): The message to be logged.
+        """
         self.emit(message.record)
 
     def _put(self, log_record):
-        labels = self.labels
+        """
+        Put a log record into the buffer.
 
-        for key in self.labelKeys:
-            if key in log_record.keys():
+        Args:
+            log_record (dict): The formatted log record.
+        """
+        labels = self.labels.copy()
+
+        for key in self.label_keys:
+            if key in log_record:
                 labels[key] = log_record[key]
 
         self.buffer.put(LogLine(labels, log_record))
 
 
 class LogLine:
+    """
+    Represents a single log line with associated labels.
+
+    Attributes:
+        labels (dict): Labels associated with the log line.
+        key (str): A unique key generated from the labels.
+        line (str): The actual log line content.
+    """
+
     def __init__(self, labels, line):
+        """
+        Initialize a LogLine object.
+
+        Args:
+            labels (dict): Labels associated with the log line.
+            line (str): The actual log line content.
+        """
         self.labels = labels
-        self.key = self.key_from_lables(labels)
+        self.key = self._key_from_labels(labels)
         self.line = line
 
-    def key_from_lables(self, labels):
+    @staticmethod
+    def _key_from_labels(labels):
+        """
+        Generate a unique key from the labels.
+
+        Args:
+            labels (dict): Labels to generate the key from.
+
+        Returns:
+            str: A unique key generated from the labels.
+        """
         key_list = sorted(labels.keys())
         return "_".join(key_list)
