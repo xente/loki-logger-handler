07d520ea56711c9ce318df865c009845b2383dc0
Roman505050 <myhun59@gmail.com>
2025-01-14 15:00:10 +0200

    Exclude test packages from distribution in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/xente/loki-logger-handler",
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests*", "tests.*"]),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
