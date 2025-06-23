5d771419bda4781ca06cab897d4a42fb22f207ea
jerakin <the_rensvind@hotmail.com>
2025-06-23 13:38:57 +0200

    chore/update-docs-typos

--- loki_logger_handler/loki_logger_handler.py
+++ loki_logger_handler/loki_logger_handler.py
@@ -42,18 +42,18 @@ class LokiLoggerHandler(logging.Handler):
 
         Args:
             url (str): The URL of the Loki server.
-            labels (dict): Default labels for the logs.
-            label_keys (dict, optional): Specific log record keys to extract as labels. Defaults to None.
+            labels (dict): A dictionary of labels to attach to each log message.
+            label_keys (dict, optional): A dictionary of keys to extract from each log message and use as labels. Defaults to None.
             auth (tuple, optional): Basic authentication credentials for the Loki request. Defaults to None.
             additional_headers (dict, optional): Additional headers for the Loki request. Defaults to None.
             message_in_json_format (bool): Whether to format log values as JSON.
-            timeout (int, optional): Timeout interval for flushing logs. Defaults to 10 seconds.
-            compressed (bool, optional): Whether to compress the logs using gzip. Defaults to True.
-            default_formatter (logging.Formatter, optional): Formatter for the log records. If not provided, LoggerFormatter or LoguruFormatter will be used.
-            enable_self_errors (bool, optional): Set to True to show Hanlder errors on console. Default False
+            timeout (int, optional): Timeout interval for flushing logs in seconds. Defaults to 10 seconds.
+            compressed (bool, optional): Whether to compress the logs before sending them using gzip. Defaults to True.
+            default_formatter (logging.Formatter, optional): Formatter for the log records. If not provided, `LoggerFormatter` or`LoguruFormatter` will be used.
+            enable_self_errors (bool, optional): Set to True to show Handler errors on console. Default False
             enable_structured_loki_metadata (bool, optional):  Whether to include structured loki_metadata in the logs. Defaults to False. Only supported for Loki 3.0 and above
             loki_metadata (dict, optional): Default loki_metadata values. Defaults to None. Only supported for Loki 3.0 and above
-            loki_metadata_keys (arrray, optional): Specific log record keys to extract as loki_metadata. Only supported for Loki 3.0 and above
+            loki_metadata_keys (array, optional): Specific log record keys to extract as loki_metadata. Only supported for Loki 3.0 and above
         """
         super(LokiLoggerHandler, self).__init__()
 
@@ -86,7 +86,7 @@ class LokiLoggerHandler(logging.Handler):
 
         self.message_in_json_format = message_in_json_format
 
-        # Halndler working with errors
+        # Handler working with errors
         self.error = False
         self.enable_structured_loki_metadata = enable_structured_loki_metadata
         self.loki_metadata = loki_metadata
